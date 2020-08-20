3c5e04e98f0f76ad8992aa55f3c53bb2737363e1
Alexandre <alexsantsjrr@gmail.com>
2020-08-19 22:43:41 -0300

    Updating build

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,24 @@
+const Category = require("../models/Product")
+
 module.exports = {
     create(req, res) {
         return res.render("products/create.njk")
+    },
+    async post(req, res){
+        const keys = Object.keys(req.body)
+
+        for (key of keys){
+            if(req.body[key] == ""){
+                return res.send("Please, fill all fields!")
+            }
+        }
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].productId
+
+        results = await Category.all()
+        const categories = results.rows
+    
+        return res.render("products/create.njk", {productId, categories})
     }
 }
